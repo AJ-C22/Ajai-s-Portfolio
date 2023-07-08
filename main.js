b56d58e9dc8944aa557b27039367a1a3ca162cf6
AJ-C22 <114104270+AJ-C22@users.noreply.github.com>
2023-07-08 14:42:31 -0400

    Added typewriter animation

--- main.js
+++ main.js
@@ -22,6 +22,19 @@ function openAbout(){
 
 function closeAbout(){
     about.classList.remove("open-about");
-
 }
+
+var messageArray = ["Aspiring Data Scientist / Developer"];
+var textPosition = 0;
+var speed = 80;
+
+typewriter = () => {
+    document.querySelector('#message').
+    innerHTML = messageArray[0].substring(0,textPosition) + "<span>\u25ae</span>"
+
+    if (textPosition++ != messageArray[0].length)
+        setTimeout(typewriter, speed);
+}
+
+window.addEventListener("load", typewriter)
 
