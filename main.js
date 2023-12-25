cbaff5258ec5d3658f0c3290697fb6d74e5763ed
AJ-C22 <114104270+AJ-C22@users.noreply.github.com>
2023-12-25 17:20:12 -0500

    Added working menu-icon

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // toggle icon navbar
-let menuIcon = document.querySelector('#menu');
+let menuIcon = document.querySelector('#menu-icon');
 let header = document.querySelector('.header');
 
-//menuIcon.onclick = () => {
-//    menuIcon.classList.toggle('bx-x');
-//    header.classList.toggle('active');
-//}
+menuIcon.onclick = () => {
+   menuIcon.classList.toggle('bx-x');
+    header.classList.toggle('active');
+}
 
 //scroll sections
 let sections = document.querySelectorAll('section');
