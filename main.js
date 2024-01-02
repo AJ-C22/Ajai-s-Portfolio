997481b9936b04cfdc03fdde9cfffcd8d4dbeea6
AJ-C22 <114104270+AJ-C22@users.noreply.github.com>
2024-01-02 14:10:17 -0500

    Added desc to experience section, new projects and fixed bottom spacing issues

--- main.js
+++ main.js
@@ -76,5 +76,21 @@ window.onscroll = () => {
 
 }
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwHENbnhth-_rg4cswEiqCUo7p8gQwvsLfmoiezkmnVGgcy1FXQgbGxn1ed4m58SXjtSA/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message Sent Successfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
 
 
